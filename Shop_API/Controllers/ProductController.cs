fd1a5dc45382d4ec55351b51de014fe1abb2093f
simonsrz <szymonslusarz01@gmail.com>
2023-04-14 18:16:47 +0200

    Add project files.

---
+++ Shop_API/Controllers/ProductController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop_API.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
---
+++ Shop_API/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Shop_API.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime creationDate { get; set; }
+        public DateTime editDateDate { get; set; }
+        public string description { get; set; }
+        public int price { get; set; }
+
+    }
+}
